7a41104893c22b8f4834b7f352b26708bc9aca11
DatalakeLab <88815311+DatalakeLab@users.noreply.github.com>
2021-08-16 18:34:54 -0300

    Update c_xgb_test.py

--- c_xgb_test.py
+++ c_xgb_test.py
@@ -1,7 +1,8 @@
 import c_xgb_test
 
 print ("XGBoost Linear Regression")
-c_xgb_test.test_xgb_regression(n_samples = 100000, n_features = 35, n_estimators = 3, depth = 15)
+c_xgb_test.test_xgb_regression(n_samples = 100000, n_features = 35, n_estimators = 10, depth = 5)
+
 
 print ("XGBoost Logistic Regression")
-c_xgb_test.test_xgb_logistic_regression(n_samples = 100000, n_features = 35, n_estimators = 3, depth = 15)
+c_xgb_test.test_xgb_logistic_regression(n_samples = 100000, n_features = 35, n_estimators = 100, depth = 10)
